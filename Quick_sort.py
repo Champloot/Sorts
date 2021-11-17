6710a6079e74786ac53d8aa0415ec9e5a600f030
Champloot <94532438+Champloot@users.noreply.github.com>
2021-11-17 16:17:54 +0300

    Update Quick_sort.py

--- Quick_sort.py
+++ Quick_sort.py
@@ -1,1 +1,18 @@
-Print (1)
+def quick_sort(arr):
+    if len(arr) < 2:
+	return arr
+    else:
+	pivot = arr[0]
+	less = [i for i in arr[1:] if i <= pivot]
+	greater = [i for i in arr[1:] if i > pivot]
+	return quick_sort(less) + [pivot] + quick_sort(greater)
+
+arr = input().split()
+n = 0
+
+for i in arr:
+    arr[n] = int(i)
+    n +=1
+
+qs = quick_sort(arr)
+print(qs)
