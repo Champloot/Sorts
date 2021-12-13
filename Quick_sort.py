e74558e5dc91a82357d98626a7c9fcba02359dc2
Archi <94532438+Champloot@users.noreply.github.com>
2021-12-13 10:26:29 +0300

    Update Quick_sort.py

--- Quick_sort.py
+++ Quick_sort.py
@@ -1,18 +1,18 @@
+from random import randint
+
 def quick_sort(arr):
     if len(arr) < 2:
-	return arr
+        return arr
     else:
-	pivot = arr[0]
+        pivot = arr[0]
 	less = [i for i in arr[1:] if i <= pivot]
 	greater = [i for i in arr[1:] if i > pivot]
 	return quick_sort(less) + [pivot] + quick_sort(greater)
 
-arr = input().split()
-n = 0
-
-for i in arr:
-    arr[n] = int(i)
-    n +=1
+arr =[]
+for _ in range(20):
+    arr.append(randint(-30, 30))
+print(arr)
 
 qs = quick_sort(arr)
 print(qs)
