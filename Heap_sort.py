33d998a39d8ae91653ef3e1a282f4100712bfd9c
Archi <94532438+Champloot@users.noreply.github.com>
2022-01-19 17:33:01 +0300

    Update Heap_sort.py

--- Heap_sort.py
+++ Heap_sort.py
@@ -1,28 +1,28 @@
 from random import randint
 
 
-def heapify(arr, top, array_length):
+def heapify(arr, top, length):
     largest = top
     left_child = 2*top+1
     right_child = 2*top+2
 
-    if left_child < array_length and arr[left_child] > arr[top]:
+    if left_child < length and arr[left_child] > arr[top]:
         largest = left_child
-    if right_child < array_length and arr[right_child] > arr[largest]:
+    if right_child < length and arr[right_child] > arr[largest]:
         largest = right_child
 
     if largest != top:
         arr[largest], arr[top] = arr[top], arr[largest]
-        heapify(arr, largest, array_length)
+        heapify(arr, largest, length)
 
 
 def heap_sort(arr):
-    array_length = len(arr)
+    length = len(arr)
 
-    for top in range(array_length//2, -1, -1):
-        heapify(arr, top, array_length)
+    for top in range(length//2, -1, -1):
+        heapify(arr, top, length)
 
-    for i in range(array_length-1, 0, -1):
+    for i in range(length-1, 0, -1):
         arr[0], arr[i] = arr[i], arr[0]
         heapify(arr, 0, i)
 
