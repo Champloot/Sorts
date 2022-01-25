5472ed7ae0f9c688044f7b02276d994a1e233b15
Archi <94532438+Champloot@users.noreply.github.com>
2022-01-25 13:41:49 +0300

    Update Shaker_sort.py

--- Shaker_sort.py
+++ Shaker_sort.py
@@ -28,9 +28,7 @@ def shaker_sort(arr):
     return arr
 
 
-arr = []
-for _ in range(20):
-    arr.append(randint(-30, 30))
+arr = [randint(-30, 30) for _ in range(20)]
 
 print(arr)
 print(shaker_sort(arr))
