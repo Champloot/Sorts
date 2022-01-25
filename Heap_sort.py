a6c7850387d6ec37c906ff25300d9fda5128711b
Archi <94532438+Champloot@users.noreply.github.com>
2022-01-25 13:40:05 +0300

    Update Heap_sort.py

--- Heap_sort.py
+++ Heap_sort.py
@@ -29,9 +29,7 @@ def heap_sort(arr):
     return arr
 
 
-arr = []
-for _ in range(20):
-    arr.append(randint(-30, 30))
+arr = [randint(-30, 30) for _ in range(20)]
 
 print(arr)
 print(heap_sort(arr))
